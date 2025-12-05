bf04a589ae8397ec50219605f68ba59326bb0585
Alex Anderson <106041065+a-anderson116@users.noreply.github.com>
2025-12-05 10:26:09 -0500

    Set up Express server with EJS and static files

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const path=require('path')
 
-app.get("/", (req,res) => {
-	res.send("Hello World!");
+var myvalue="dog";
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/one", (req, res) => {res.render('./one.html')})
+app.post("/two", function(req, res) { 
+    // For POST body (like curl -d "myvalue=cat")
+    const animal = req.body.myvalue;
+
+    console.log("animal =", animal);
+    res.render("two.html", { animal: animal });
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is Running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
